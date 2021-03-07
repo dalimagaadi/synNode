ae860e12e9bbe32d1fea6bce084b7261caf25e86
Dali <dali.magaadi@gmail.com>
2021-03-07 11:37:40 +0100

    Changing deconstructing

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ const converter = CsvToJson({
     factuurnummer: "omit"
   }
 });
-const dossierPath = "Voorbeeld dossiers import.csv";
 
-converter.fromFile("./Voorbeeld dossiers import.csv").then((source) => {
-  console.log(source);
+const dossierPath = "Voorbeeld dossiers import.csv";
+let dossierList = [];
+
+converter.fromFile(dossierPath).then((source) => {
+  let dossier = {};
+  for (let i = 0; i < source.length; i++) {
+    dossier = (({ Dossiernummer, ["openstaand saldo"]: openstaand_saldo }) => ({
+      Dossiernummer,
+      openstaand_saldo
+    }))(source[i]);
+    dossierList.push(dossier);
+  }
+  console.log(dossierList);
 });
+
+//Functie dat gebaseerd op meerdere parameters een object vult met de attributen van de source return in de 'then' van de converter
+//Parameter 1: Type object: zoals Dossier, Debiteur, Eiser
+//Parameter 2: source[i] object
+//Paramter 3: ?
+
+// Bij Parameter 1 wordt er gekeken naar pre-gedefineerde profiel/model van dat object type voor de nodige attributen
